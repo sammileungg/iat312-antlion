52e7e2ec9b380b2c6ef366288e274bcae4c48285
sammileungg <sammileungg@gmail.com>
2014-10-01 17:39:35 -0700

    prelim test player movement with arrow keys and continually moving up

---
+++ PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿///player input movement left and right, continuous movement up
+///sources: http://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/infinite-runner
+///at time 16:43
+///and in-lab playermovement code
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	void Update () {
+		Movement();
+	}
+
+	void Movement()
+	{
+	if(Input.GetKey (KeyCode.RightArrow))
+		{
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+		}
+	
+		if(Input.GetKey (KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector2.left * 4f * Time.deltaTime);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		transform.Translate(Vector2.up * 4f * Time.deltaTime);
+	}
+}
+
